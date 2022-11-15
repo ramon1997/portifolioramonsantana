81809c52df275d08e0edaa4dfa65da84d7ccee11
ramon1997 <kratosramonsantana@gmail.com>
2022-11-15 20:55:24 -0300

    add efeitos nos cards e aumentando tempo do banner

--- projetos/banner.js
+++ projetos/banner.js
@@ -1,14 +1,14 @@
 function slide1() {
     document.getElementById('id').src="imagens/som.png";
-    setTimeout("slide2()", 2000)
+    setTimeout("slide2()", 4000)
     }
 
 function slide2() {
     document.getElementById('id').src="imagens/camera.png";
-    setTimeout("slide3()", 2000)
+    setTimeout("slide3()", 4000)
     }
 
 function slide3() {
     document.getElementById('id').src="imagens/oculos.jpg";
-    setTimeout("slide1()", 2000)
+    setTimeout("slide1()", 4000)
     }
